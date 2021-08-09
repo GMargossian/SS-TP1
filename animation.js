d4c40b789c30ca35d740e36693baa5bc0d4c2298
Gabriel Margossian <gmargossian@itba.edu.ar>
2021-08-09 11:16:14 -0300

    added PBC on animation.js

--- animation.js
+++ animation.js
@@ -1,6 +1,10 @@
 var particle_list = [];
 var RC;
 
+var hasWalls;
+
+var directions = [[1,0],[0,1], [1,1], [-1,0], [0,-1], [-1,-1]];
+
 class Particle{
     constructor(id,x,y,radius,neighbours,color){
         this.id = id;
@@ -11,21 +15,40 @@ class Particle{
         this.color = color;
     }
 
-    draw(context){
+    draw(context, L){
         context.beginPath();
         context.arc( this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        
+           
         // Fill Stroke
         context.fillStyle = this.color;
         context.fill();
         context.stroke();
         context.font = "15px Arial";
-        context.fillStyle = "blue"
+        context.fillStyle = "white"
         context.textAlign = "center";
         context.textBaseline = "middle";
         context.fillText(this.id,this.x,this.y);
-
         context.closePath();
+
+        if(!this.hasWalls){
+
+            
+            for (const direc of directions) {
+                // console.log(direc);
+
+                context.beginPath();
+                context.arc( this.x + direc[0] * L, this.y + direc[1] * L, this.radius, 0, Math.PI * 2, false);
+                context.fillStyle = this.color;
+                context.fill();
+                context.stroke();
+                context.font = "15px Arial";
+                context.fillStyle = "white"
+                context.textAlign = "center";
+                context.textBaseline = "middle";
+                context.fillText(this.id,this.x + direc[0] * L, this.y + direc[1] * L);
+                context.closePath();
+            }
+        }
     }
 
     clickParticle(xMouse,yMouse){
@@ -59,19 +82,23 @@ dataSelector.addEventListener('change', (event) => {
 );
 
 function onReaderLoad(event){
-    console.log(event.target.result);
+    // console.log(event.target.result);
     var data = JSON.parse(event.target.result);
-    console.log(data['particles'])
+    // console.log(data['particles'])
+    
     scale = 70;
-    canvas.width = data['L'] * scale;
-    canvas.height = data['L'] * scale;
+     
+    canvas.width = data['L']*scale;
+    canvas.height = data['L']*scale;
     RC = data['RC'] * scale;
+    hasWalls = data['hasWalls'];
+
     particle_list = [];
     data['particles'].forEach((particle) =>{
-        console.log(particle)
+        // console.log(particle)
         let new_particle = new Particle(particle['id'],particle['posX'] * scale, particle['posY'] * scale, particle['radius'] * scale, particle['neighbours'], "green");
         // Generate x and y values between 0 to 140 considering 30 pt radius
-        new_particle.draw(context);
+        new_particle.draw(context, canvas.width);
         particle_list.push(new_particle);
     })
     drawWindowBorders();
@@ -85,9 +112,9 @@ function resetParticleColor(){
 
 function getClickedParticle(x,y){
     for(let particle of particle_list){
-        console.log(particle.id + ": " + particle.clickParticle(x,y));
+        // console.log(particle.id + ": " + particle.clickParticle(x,y));
         if(particle.clickParticle(x,y)){
-            console.log("returning: " + particle)
+            // console.log("returning: " + particle)
             return particle;
         }
     }
@@ -116,23 +143,32 @@ canvas.addEventListener('click', (event) =>{
     // iterate particles in particles:
     if(particle_list.length !== 0){
         let clicked = getClickedParticle(x,y);
-        console.log(clicked);
+        // console.log(clicked);
         if(clicked !== null){
-            console.log("particle clicked: " + clicked.id);
+            // console.log("particle clicked: " + clicked.id);
             resetParticleColor();
             clicked.color = "red";
-
-
 
             const neighbours = particle_list.filter(p => clicked.neighbours.includes(p.id))
             neighbours.forEach(n=> n.color = "pink");
             context.clearRect(0, 0, canvas.width, canvas.height);
+            
             context.beginPath();
-            context.arc( clicked.x, clicked.y, clicked.radius + RC, 0, Math.PI * 2, false); //TODO: Cambiar al RC
+            context.arc( clicked.x, clicked.y, clicked.radius + RC, 0, Math.PI * 2, false); 
             context.stroke();
             context.closePath();
+
+            if(!this.hasWalls){
+                for (const direc of directions) {
+                    context.beginPath();
+                    context.arc( clicked.x + direc[0] * canvas.width, clicked.y + direc[1] * canvas.width, clicked.radius + RC, 0, Math.PI * 2, false); 
+                    context.stroke();
+                    context.closePath();
+                }
+            }
+
             drawWindowBorders();
-            particle_list.forEach(p => p.draw(context));
+            particle_list.forEach(p => p.draw(context, canvas.width));
         }
     }
 
